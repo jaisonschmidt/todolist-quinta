a64d85df028e33ada5c03acf77bd83770d66da02
Jaison <jaisonschmidt@gmail.com>
2023-09-28 21:05:45 -0300

    feat: Cria função createTask

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
-console.log("Script.js funcionando");
+const tasks = [];
+
+function newId() {
+    // Gerar IDs entre 0 e 999
+    return Math.floor(Math.random() * 1000); 
+}
+
+function createTask(taskTitle, taskDescription = "") {
+    let id = newId();
+    
+    let task = {
+        id,
+        taskTitle: taskTitle,
+        taskDescription: taskDescription,
+    }
+
+    tasks.push(task);
+    // renderTasks();
+    return task;
+}
+
+
